f9266fbdae39529155c85154f25d7bd7d017c703
bereg2k <bere2k@gmail.com>
2020-06-01 14:29:37 +0300

    Добавлена поддержка chromedriver для Windows и Linux

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,6 +1,7 @@
 package base;
 
 import net.thucydides.core.annotations.Managed;
+import org.apache.commons.lang3.SystemUtils;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 
@@ -14,8 +15,29 @@ public abstract class BaseTest {
 
     @Before
     public void initWebDriver() {
-        String pathToDriverBinary = System.getProperty("user.dir") + "/driver/chromedriver";
-        System.setProperty("webdriver.chrome.driver", pathToDriverBinary);
+        System.setProperty("webdriver.chrome.driver", driverPathForOs());
         driver.manage().window().maximize();
+    }
+
+    /**
+     * Определение используемого бинарного файла для веб-драйвера в зависимости от используемой ОС.
+     *
+     * @return путь к проектной папке с нужным веб-драйвером
+     * @throws IllegalArgumentException если для ОС пользователя нет веб-драйверов в проекте
+     */
+    private static String driverPathForOs() {
+        String pathToDriverBinary;
+
+        if (SystemUtils.IS_OS_MAC) {
+            pathToDriverBinary = "/driver/chromedriver_mac";
+        } else if (SystemUtils.IS_OS_WINDOWS) {
+            pathToDriverBinary = "/driver/chromedriver.exe";
+        } else if (SystemUtils.IS_OS_LINUX) {
+            pathToDriverBinary = "/driver/chromedriver_linux";
+        } else {
+            throw new IllegalArgumentException("No available WebDriver for current OS!");
+        }
+
+        return System.getProperty("user.dir") + pathToDriverBinary;
     }
 }
